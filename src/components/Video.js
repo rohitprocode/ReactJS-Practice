569d1e2f33c26feaa8b1e1ab401b952d92b91a0f
rohitprocode <rohit990797@gmail.com>
2023-10-20 17:38:31 +0530

    verified logo img

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import verified_logo from './verified_logo.png'
 import './Video.css'
-export default function Video({ title, Channel, Views, time }) {
+export default function Video({ title, Channel, Views, time, verifiedData }) {
+    let newJSXverified;
+    if(verifiedData){
+    newJSXverified = <div className='channel'>{Channel}<img src={verified_logo} alt="verified_log" /></div>
+    }else{
+    newJSXverified = <div className='channel'>{Channel}</div>
+    }
     return (
         <>
             <div className='container' >
@@ -8,9 +15,9 @@ export default function Video({ title, Channel, Views, time }) {
                     <img src="https://wallpapers.com/images/high/textured-solid-grey-image-vignette-fogxulntlildtj4p.webp" alt="Flower Image" />
                 </div>
                 <div className="title">{title}</div>
-                <div className="channel">{Channel}</div>
+                {newJSXverified}
                 <div className="views">
-                    {Views} Views <span>.</span> {time}
+                    {Views} Views<span>.</span> {time}
                 </div>
             </div>
         </>
