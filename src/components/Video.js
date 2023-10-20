ee39c01a0a647e1469e8fcd371fa2b5a4baf7389
rohitprocode <rohit990797@gmail.com>
2023-10-20 20:44:41 +0530

    Shortcurcuit Method &&

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,7 +2,7 @@ import React from 'react'
 import verified_logo from './verified_logo.png'
 import './Video.css'
 export default function Video({ title, Channel, Views, time, verifiedData }) {
-   
+    
     return (
         <>
             <div className='container' >
@@ -10,9 +10,10 @@ export default function Video({ title, Channel, Views, time, verifiedData }) {
                     <img src="https://wallpapers.com/images/high/textured-solid-grey-image-vignette-fogxulntlildtj4p.webp" alt="Flower Image" />
                 </div>
                 <div className="title">{title}</div>
-    <div className='channel'>{Channel} {verifiedData ? <img src={verified_logo} alt="verified_log"/> : null}</div>
+                {/* <div className='channel'>{Channel} {verifiedData ? <img src={verified_logo} alt="verified_logo"/> : null}</div> */}
+                <div className="channel">{Channel}{verifiedData && <img src={verified_logo} alt='verified_logo'/>}</div>
                 <div className="views">
-                    {Views} Views<span>.</span> {time}
+                    {Views} Views<span>.</span>{time}
                 </div>
             </div>
         </>
