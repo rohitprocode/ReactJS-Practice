06556c2819341a0ac09bf022999b196d060c4775
rohitprocode <rohit990797@gmail.com>
2023-10-20 22:04:23 +0530

    Store data in separate folder/file

--- src/App.js
+++ src/App.js
@@ -1,46 +1,6 @@
 import './App.css';
 import Video from './components/Video';
-
-const Videos = [{
-  title: 'Wordpress Developer',
-  Channel: 'rohitech',
-  Views: '1M',
-  time: '6 months ago',
-  verifiedData: false,
-  id:1
-},
-{
-  title: 'React Developer',
-  Channel: 'rohitech',
-  Views: '149K',
-  time: '4 months ago',
-  verifiedData: true,
-  id:2
-},
-{
-  title: 'Node Developer',
-  Channel: 'rohitech',
-  Views: '849K',
-  time: '5 months ago',
-  verifiedData: true,
-  id:3
-},
-{
-  title: 'Mongo DB Developer',
-  Channel: 'rohitech',
-  Views: '49K',
-  time: '9 months ago',
-  verifiedData: false,
-  id:4
-},
-{
-  title: 'Python Developer',
-  Channel: 'rohitech',
-  Views: '999K',
-  time: '12 months ago',
-  verifiedData: true,
-  id:5
-}]
+import Videos from './data/data'
 
 function App() {
   return (
