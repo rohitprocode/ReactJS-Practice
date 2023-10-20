fb9179f507f41d2e4f369219b327df8299b3d619
rohitprocode <rohit990797@gmail.com>
2023-10-20 17:39:38 +0530

    verified logo img

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ const obj={
   title:'Wordpress Developer',
   Channel:'rohitech',
   Views:'1M',
-  time:'6 months ago'
+  time:'6 months ago',
+  verifiedData: true
 }
 
 function App() {
@@ -17,25 +18,29 @@ function App() {
       title={"React Js Tutorial"} 
       Channel={'rohitprocode'} 
       Views={'149K'} 
-      time={"4 months ago"} 
+      time={"4 months ago"}
+      verifiedData={true} 
       />
       <Video 
       title={"Node Js Tutorial"} 
       Channel={'rohitprocode'} 
       Views={'849K'} 
-      time={"5 months ago"} 
+      time={"5 months ago"}
+      verifiedData={false} 
       />
       <Video 
       title={"Mongo DB Tutorial"} 
       Channel={'rohitprocode'} 
       Views={'49K'} 
-      time={"8 months ago"} 
+      time={"8 months ago"}
+      verifiedData={false} 
       />
       <Video 
       title={"Python Tutorial"} 
       Channel={'rohitprocode'} 
       Views={'885K'} 
-      time={"11 months ago"} 
+      time={"11 months ago"}
+      verifiedData={true} 
       />
 
     </div>
