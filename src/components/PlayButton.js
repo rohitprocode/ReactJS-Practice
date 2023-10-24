cde00a288a12dc3e60931d54a4e5a6967773d46b
rohitprocode <rohit990797@gmail.com>
2023-10-24 17:54:07 +0530

    Different function in same click

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './PlayButton.css'
 
-function PlayButton({Message,children,Action}) {
+function PlayButton({ Message, children, onStart, onStop }) {
+  let playing = true;
+  function clickHandler() {
+    if (playing){
+      onStart()
+      playing = false
+    }else{
+      onStop()
+      playing = true;
+    }
+  }
+
   return (
-      <button onClick={()=>(Message)}>{children}</button>
+    <button onClick={clickHandler}>{children}</button>
   )
 }
 
