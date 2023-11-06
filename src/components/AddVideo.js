8026971cfa3a2ea7efa6e937e260bb0ef18e9fba
rohitprocode <rohit990797@gmail.com>
2023-11-06 20:08:36 +0530

    Set title and views by input

--- src/components/AddVideo.js
+++ src/components/AddVideo.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import './AddVideo.css'
 
 function AddVideo(){
-
+    const [video,setVideo] = useState({
+        Channel: 'rohitech',
+        time: '6 months ago',
+        verifiedData: false
+    })
     function clickHandler(e){
         e.stopPropagation();
         console.log(e.target.value , e.target.name);
+        setVideo({...video,
+        [e.target.name] : e.target.value})
+        console.log(video)
     }
     return(
         <form>
