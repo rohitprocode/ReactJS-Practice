1484c6f65e0a12a02e4bd99ae1faf3a6103e0c8c
rohitprocode <rohit990797@gmail.com>
2023-11-14 14:25:56 +0530

    Add New Video

--- src/components/AddVideo.js
+++ src/components/AddVideo.js
@@ -1,29 +1,31 @@
 import { useState } from 'react';
 import './AddVideo.css'
 
-function AddVideo({addVideo}){
-    const [video,setVideo] = useState({
+function AddVideo({ addVideo }) {
+    const [video, setVideo] = useState({
         Channel: 'rohitech',
         time: '6 months ago',
-        verifiedData: false
+        verifiedData: true
     })
-    function clickHandler(e){
+    function clickHandler(e) {
         // e.stopPropagation();
-        console.log(e.target.value , e.target.name);
-        setVideo({...video,
-        [e.target.name] : e.target.value})
-        // console.log(video)
+        // console.log(e.target.value, e.target.name);
+        setVideo({
+            ...video,
+            [e.target.name]: e.target.value
+        })
+        console.log(video)
     }
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
         addVideo(video)
         // console.log(video)
     }
-    
-    return(
+
+    return (
         <form>
             <input type="text" name="title" onChange={clickHandler} placeholder='Title' />
-            <input type="text" name='views' onChange={clickHandler} placeholder='Views'/>
+            <input type="text" name='Views' onChange={clickHandler} placeholder='Views' />
             <button onClick={handleSubmit} >Add Video</button>
         </form>
     )
