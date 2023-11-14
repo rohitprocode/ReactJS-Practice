69112a34f6bc65a9b2e63c2f823554e1e3b1c2bd
rohitprocode <rohit990797@gmail.com>
2023-11-14 15:25:15 +0530

    Clear input field

--- src/components/AddVideo.js
+++ src/components/AddVideo.js
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import './AddVideo.css'
 
+const initialState = {
+    Channel: 'rohitech',
+    time: '6 months ago',
+    verifiedData: true,
+    title: '',
+    Views: ''
+}
+
 function AddVideo({ addVideo }) {
-    const [video, setVideo] = useState({
-        Channel: 'rohitech',
-        time: '6 months ago',
-        verifiedData: true
-    })
+    const [video, setVideo] = useState(initialState)
     function clickHandler(e) {
         // e.stopPropagation();
         // console.log(e.target.value, e.target.name);
@@ -14,18 +18,19 @@ function AddVideo({ addVideo }) {
             ...video,
             [e.target.name]: e.target.value
         })
-        console.log(video)
+        // console.log(video)
     }
     function handleSubmit(e) {
         e.preventDefault();
         addVideo(video)
+        setVideo(initialState)
         // console.log(video)
     }
 
     return (
         <form>
-            <input type="text" name="title" onChange={clickHandler} placeholder='Title' />
-            <input type="text" name='Views' onChange={clickHandler} placeholder='Views' />
+            <input type="text" name="title" onChange={clickHandler} placeholder='Title' value={video.title} />
+            <input type="text" name='Views' onChange={clickHandler} placeholder='Views' value={video.Views} />
             <button onClick={handleSubmit} >Add Video</button>
         </form>
     )
