a951416e5bb8c37ba90d6a24f304a7f7289d92ed
rohitprocode <rohit990797@gmail.com>
2023-12-31 23:59:34 +0530

    createContext and useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,50 @@
-import { useReducer, useState } from 'react';
-import './App.css';
-import videoDB from './data/data'
-import AddVideo from './components/AddVideo';
-import VideoList from './components/VideoList';
-import { type } from '@testing-library/user-event/dist/type';
+import { useContext, useReducer, useState } from "react";
+import "./App.css";
+import videoDB from "./data/data";
+import AddVideo from "./components/AddVideo";
+import VideoList from "./components/VideoList";
+import { type } from "@testing-library/user-event/dist/type";
+import ThemeContext from "./components/context/ThemeContext";
 
 function App() {
-  
-  const [editableVideo,setEditableVideo] = useState(null)
+  const [editableVideo, setEditableVideo] = useState(null);
 
-  
-  //videos parameter is a state
+  function videosReducer(videos, action) {
+    switch (action.type) {
+      case "ADD":
+        return [...videos, { ...action.payload, id: videos.length + 1 }];
 
-  function videosReducer(videos,action){
-    switch(action.type){
-      case 'ADD' : 
-      return [...videos,
-        { ...action.payload, id: videos.length + 1 }
-        ];
-        
-        case 'DELETE' :
-          return videos.filter(deleVideo => deleVideo.id !== action.payload)
+      case "DELETE":
+        return videos.filter((deleVideo) => deleVideo.id !== action.payload);
 
-          case 'UPDATE' :
-            const index = videos.findIndex(v=>v.id===action.payload.id)
-            const newVideos = [...videos]
-            newVideos.splice(index,1,action.payload)
-            setEditableVideo(null)
-            return newVideos
-
-        default : 
-        return videos
+      case "UPDATE":
+        const index = videos.findIndex((v) => v.id === action.payload.id);
+        const newVideos = [...videos];
+        newVideos.splice(index, 1, action.payload);
+        setEditableVideo(null);
+        return newVideos;
+      default:
+        return videos;
     }
   }
 
-  const [videos,dispatch] = useReducer(videosReducer,videoDB)
+  const [videos, dispatch] = useReducer(videosReducer, videoDB);
 
-  // const [videos, setVideos] = useState(videoDB)
+  const themeContext = useContext(ThemeContext)
 
-
-  function addVideos(video) {
-    // dispatch({type : 'ADD' , payload:video })
-    // setVideos(
-    //   [...videos,
-    //   { ...video, id: videos.length + 1 }
-    //   ]);
-  }
-
-  function deleteVideos(id) {
-    // dispatch({type : 'DELETE', payload:id})
-    // setVideos(
-    //   videos.filter(deleVideo => deleVideo.id !== id)
-    // )
-  }
-
-  function editVideos(id){
-    setEditableVideo(videos.find(editzz => editzz.id === id))
-  }
-
-  function updateVideo(video){
-    // console.log(video)
-    // dispatch({type : 'UPDATE', payload:video})
-    // const index = videos.findIndex(v=>v.id===video.id)
-    // const newVideos = [...videos]
-    // newVideos.splice(index,1,video)
-    // setVideos(newVideos)
+  function editVideos(id) {
+    setEditableVideo(videos.find((editzz) => editzz.id === id));
   }
 
   return (
-    <div className="App" onClick={() => console.log("App Clicked")} >
-      <AddVideo dispatch={dispatch} editableVideo={editableVideo} ></AddVideo>
-      <VideoList videos={videos} dispatch={dispatch} editVideos={editVideos} ></VideoList>
-      <div style={{ clear: 'both' }} >
-      </div>
+    <div className={`App ${themeContext}`} onClick={() => console.log("App Clicked")}>
+      <AddVideo dispatch={dispatch} editableVideo={editableVideo}></AddVideo>
+      <VideoList
+        videos={videos}
+        dispatch={dispatch}
+        editVideos={editVideos}
+      ></VideoList>
+      <div style={{ clear: "both" }}></div>
     </div>
   );
 }
---
+++ src/components/context/ThemeContext.jsx
@@ -0,0 +1,5 @@
+import React, { createContext } from 'react'
+
+const ThemeContext = createContext('darkmode');
+
+export default ThemeContext
